7e7fc19b606979df3457497f2d8e2b4085644a20
Armando Faz <armfazh@users.noreply.github.com>
2023-02-24 06:33:56 -0800

    add json and text writers (#1)

--- genkat.py
+++ genkat.py
@@ -6,19 +6,7 @@ KAT implementation for NIST (based on TestVectorGen.zip)
 
 import ascon
 import sys
-
-def kat_print_str(fp, string):
-    "print string to file pointer fp"
-    fp.write("{string}\n".format(string=string))
-
-def kat_print_val(fp, label, value):
-    "print 'label = value' for integer 'value' with kat_print to file pointer fp"
-    kat_print_str(fp, "{label} = {value}".format(label=label, value=value))
-
-def kat_print_hex(fp, label, data, length):
-    "print 'label = data[:length]' for string 'data' with kat_print to file pointer fp"
-    assert len(data) >= length
-    kat_print_str(fp, "{label} = {data}".format(label=label, data=data[:length].hex().upper()))
+from writer import MultipleWriter
 
 def kat_bytes(length):
     return bytes(bytearray([i % 256 for i in range(length)]))
@@ -31,7 +19,7 @@ def kat_aead(variant):
     klen = 20 if variant == "Ascon-80pq" else 16  # =CRYPTO_KEYBYTES
     nlen = 16  # =CRYPTO_NPUBBYTES
     tlen = 16  # <=CRYPTO_ABYTES
-    filename = "LWC_AEAD_KAT_{klenbits}_{nlenbits}.txt".format(klenbits=klen*8, nlenbits=nlen*8)
+    filename = "LWC_AEAD_KAT_{klenbits}_{nlenbits}".format(klenbits=klen*8, nlenbits=nlen*8)
     assert variant in ["Ascon-128", "Ascon-128a", "Ascon-80pq"]
 
     key   = kat_bytes(klen)
@@ -39,41 +27,42 @@ def kat_aead(variant):
     msg   = kat_bytes(MAX_MESSAGE_LENGTH)
     ad    = kat_bytes(MAX_ASSOCIATED_DATA_LENGTH)
 
-    with open(filename, "w") as fp:
+    with MultipleWriter(filename) as w:
         count = 1
         for mlen in range(MAX_MESSAGE_LENGTH+1):
             for adlen in range(MAX_ASSOCIATED_DATA_LENGTH+1):
-                kat_print_val(fp, "Count", count)
+                w.open()
+                w.append("Count",count)
                 count += 1
-                kat_print_hex(fp, "Key", key, klen)
-                kat_print_hex(fp, "Nonce", nonce, nlen)
-                kat_print_hex(fp, "PT", msg, mlen)
-                kat_print_hex(fp, "AD", ad, adlen)
+                w.append("Key", key, klen)
+                w.append("Nonce", nonce, nlen)
+                w.append("PT", msg, mlen)
+                w.append("AD", ad, adlen)
                 ct = ascon.ascon_encrypt(key, nonce, ad[:adlen], msg[:mlen], variant)
                 assert len(ct) == mlen + tlen
-                kat_print_hex(fp, "CT", ct, len(ct))
-                kat_print_str(fp, "")
+                w.append("CT", ct, len(ct))
                 msg2 = ascon.ascon_decrypt(key, nonce, ad[:adlen], ct, variant)
                 assert len(msg2) == mlen
                 assert msg2 == msg[:mlen]
-        
+                w.close()
 
 def kat_hash(variant="Ascon-Hash"):
     MAX_MESSAGE_LENGTH = 1024
     hlen = 32  # =CRYPTO_BYTES
-    filename = "LWC_HASH_KAT_{hlenbits}.txt".format(hlenbits=hlen*8)
+    filename = "LWC_HASH_KAT_{hlenbits}".format(hlenbits=hlen*8)
     assert variant in ["Ascon-Xof", "Ascon-Xofa", "Ascon-Hash", "Ascon-Hasha"]
 
     msg = kat_bytes(MAX_MESSAGE_LENGTH)
-    with open(filename, "w") as fp:
+    with MultipleWriter(filename) as w:
         count = 1
         for mlen in range(MAX_MESSAGE_LENGTH+1):
-            kat_print_val(fp, "Count", count)
+            w.open()
+            w.append("Count", count)
             count += 1
-            kat_print_hex(fp, "Msg", msg, mlen)
+            w.append("Msg", msg, mlen)
             tag = ascon.ascon_hash(msg[:mlen], variant, hlen)
-            kat_print_hex(fp, "MD", tag, hlen)
-            kat_print_str(fp, "")
+            w.append("MD", tag, hlen)
+            w.close()
 
 
 def kat(variant):
